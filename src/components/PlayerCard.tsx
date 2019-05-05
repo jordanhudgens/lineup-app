0fa502a399d7b09193d9c042964f62399827e748
jordanhudgens <jordan@devcamp.com>
2019-05-04 18:26:39 -0700

    Updated the params to match with the lineup card and added sample data

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 interface IPlayerCardProps {
   player: {
     name: string;
-    number: string;
+    position: string;
     data: Array<string>;
   };
 }
--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -6,15 +6,22 @@ interface ITeamCardProps {
 }
 
 const TeamCard = (props: ITeamCardProps) => {
-  const players = [
-    { id: "1", name: "Jon Snow", number: "4", data: [".123", ".748"] },
-    { id: "2", name: "Jon Snow", number: "6", data: [".500", ".123"] }
+  const batters = [
+    { id: "1", name: "SOGARD", position: "4", data: [".500", ".400"] },
+    { id: "2", name: "URENA", position: "6", data: [".571", ".235"] },
+    { id: "3", name: "GRICHUK", position: "9", data: [".250", ".197"] },
+    { id: "4", name: "SMOAK", position: "3", data: [".286", ".326"] },
+    { id: "5", name: "TELLEZ", position: "DH", data: [".125", ".261"] },
+    { id: "6", name: "DRURY", position: "5", data: [".182", ".204"] },
+    { id: "7", name: "MCKINNEY", position: "7", data: [".250", ".245"] },
+    { id: "8", name: "BRITO", position: "8", data: [".000", ".045"] },
+    { id: "9", name: "MAILE", position: "2", data: [".083", ".286"] }
   ];
 
   const playerList = () => {
-    if (players.length > 0) {
-      return players.map(player => {
-        return <PlayerCard key={player.id} player={player} />;
+    if (batters.length > 0) {
+      return batters.map(batter => {
+        return <PlayerCard key={batter.id} player={batter} />;
       });
     }
   };
