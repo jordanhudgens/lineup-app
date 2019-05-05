aa5ea9f9118dbf5c586c1febb370c9bd4853e020
jordanhudgens <jordan@devcamp.com>
2019-05-04 17:45:11 -0700

    Defined structure for the lineup card

--- src/components/LineupCard.tsx
+++ src/components/LineupCard.tsx
@@ -1,5 +1,16 @@
 import * as React from "react";
 
+// LineupCard
+// score
+// - 2 team cards
+//   - date
+//   - location
+//   - 9 batters
+//   - Bench
+//     - bench players
+//   - current pitcher
+//   - bullpen
+//     - pitcher
 const LineupCard = () => {
   return <div>Lineuper</div>;
 };
